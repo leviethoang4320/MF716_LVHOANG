269541ba953b22851c125969c54c6ac79eae37bb
leviethoang4320 <lehoainama5k55@gmail.com>
2021-02-23 00:17:30 +0700

    update vue router

--- FrontEnd/src/router/index.js
+++ FrontEnd/src/router/index.js
@@ -7,6 +7,14 @@ import Store from '../views/dictionary/chain_stores/Store.vue'
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    name: 'Store',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: Store
+  },
   
   {
     path: '/dictionary/store',
